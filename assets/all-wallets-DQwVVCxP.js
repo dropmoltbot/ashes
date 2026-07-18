d3d61e2dfb1a571f3f60b4b1f68e9a6ace7f1e4a
dropmoltbot <dropxtor@proton.me>
2026-07-18 19:10:06 +0200

    v3: SVG gothic logo + Framer Motion + icon buttons, animated ring

---
+++ assets/all-wallets-DQwVVCxP.js
@@ -0,0 +1,6 @@
+import{w as o}from"./core-B3AIyKaa.js";import"./index-BZxdANIJ.js";import"./events-DO4FmKo7.js";import"./index.es-vqH7GcuZ.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{p as allWalletsSvg};
