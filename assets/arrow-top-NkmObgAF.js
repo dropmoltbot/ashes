d8e737b1e174b2c97f840029450cf5e918240960
dropmoltbot <dropxtor@proton.me>
2026-07-18 17:56:26 +0200

    fix: WagmiProvider wraps useAccount (Inner component)

--- assets/arrow-top-NkmObgAF.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-3cjS_P2f.js";import"./index-DMOX8PcH.js";import"./events-CQAG7fpe.js";import"./index.es-DJ_GN5-Z.js";import"./index-nibyPLVP.js";const t=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M7 13.99a1 1 0 0 1-1-1V5.4L3.54 7.86a1 1 0 0 1-1.42-1.41L6.3 2.28a1 1 0 0 1 1.41 0l4.17 4.17a1 1 0 1 1-1.41 1.41L8 5.4v7.59a1 1 0 0 1-1 1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as arrowTopSvg};
---
+++ assets/cursor-BlvmckYc.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-l07kmkQ4.js";import"./index-DaVSf_1j.js";import"./events-BY2-Bq8p.js";import"./index.es-DNAWInVB.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
