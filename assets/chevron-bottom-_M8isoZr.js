22f7754ac201864590bc51b9e14357364b8cc602
dropmoltbot <dropxtor@proton.me>
2026-07-18 20:30:38 +0200

    v6: projectId WalletConnect + detailed gothic AshesLogo (heraldic)

--- assets/chevron-bottom-_M8isoZr.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-CSC8dfsm.js";import"./index-kLlOWh48.js";import"./events-DkKTQ5C0.js";import"./index.es-fLiy4LrV.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronBottomSvg};
