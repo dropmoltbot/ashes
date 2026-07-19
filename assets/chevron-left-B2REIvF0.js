8836a63f4a3801fbc9982cf60dd0f97e7f748154
dropmoltbot <dropxtor@proton.me>
2026-07-19 14:13:35 +0200

    v18: fix dashboard après connect + flatten GPU + perf

---
+++ assets/chevron-left-B2REIvF0.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-DtMl2i5w.js";import"./index-C_5FTumN.js";import"./events-CXwDkDQg.js";import"./index.es-CBo7yKGk.js";import"./index-nibyPLVP.js";import"./fallback-_e1w9Xjh.js";const m=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{m as chevronLeftSvg};
