799e96c839f29f8d18511c7b6ec1dccb40a8afa9
dropmoltbot <dropxtor@proton.me>
2026-07-19 13:54:47 +0200

    v16: stress fix - timeout cap 365d + frontend input validation

--- assets/cursor-Dc3qQUjS.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-yG-naGLf.js";import"./index-DYKny7AS.js";import"./events-BlbUMsuz.js";import"./index.es-iV_H0RZy.js";import"./index-nibyPLVP.js";import"./fallback-DDCOYBqL.js";const e=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{e as cursorSvg};
