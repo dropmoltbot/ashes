8b27111735e9d67902f70d972bbb911111cdd75a
dropmoltbot <dropxtor@proton.me>
2026-07-19 13:09:31 +0200

    v15: dynamic per-user contract (Forge Your Switch + Deploy)

--- assets/cursor-FSFcsFqh.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-BxmMs0rk.js";import"./index-Bpijg0KI.js";import"./events-D6nAjvdV.js";import"./index.es-CZGhI7Ih.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
