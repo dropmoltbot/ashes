c9b9ea8528b0677b2b8ee71f4d9bb631cd43feb4
dropmoltbot <dropxtor@proton.me>
2026-07-19 14:23:34 +0200

    v18 final: fix dashboard + 24 flakes + no GPU + 30s refetch + loading state

---
+++ assets/cursor-LNZih6xJ.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-DNdOtAKG.js";import"./index-DPs9QeLz.js";import"./events-BYTal3ty.js";import"./index.es-Dn5zpgDn.js";import"./index-nibyPLVP.js";import"./fallback-BjkC0RYx.js";const e=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{e as cursorSvg};
