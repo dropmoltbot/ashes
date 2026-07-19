e0553d9085f3811b138d943c20928871aaade3bb
dropmoltbot <dropxtor@proton.me>
2026-07-19 12:19:50 +0200

    v14: gas 200k explicit + functionName direct fix

---
+++ assets/chevron-top-B3ZsY-NX.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-BxmMs0rk.js";import"./index-Bpijg0KI.js";import"./events-D6nAjvdV.js";import"./index.es-CZGhI7Ih.js";import"./index-nibyPLVP.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{t as chevronTopSvg};
