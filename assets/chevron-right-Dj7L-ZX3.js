371e7a0b709d7f4309e24f440e62d78a52129b13
dropmoltbot <dropxtor@proton.me>
2026-07-19 15:39:29 +0200

    v19 elite: fix useContractSet + Forge receipt hook + inline styles→classes + og-image + manifest + canonical + a11y focus + scrollbar gothique

---
+++ assets/chevron-right-Dj7L-ZX3.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-LH6E-UHy.js";import"./index-C0zjlOjx.js";import"./events-BFY7EK29.js";import"./index.es-HlWAKGh9.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as chevronRightSvg};
