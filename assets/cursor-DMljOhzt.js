8b28cc2a66f4b989d67f78b6b927fcf4e91d176c
dropmoltbot <dropxtor@proton.me>
2026-07-19 21:53:09 +0200

    v23: Deployed event + multi-fallback contract address extraction from receipt

---
+++ assets/cursor-DMljOhzt.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-BJWLayIl.js";import"./index-DlndbomD.js";import"./events-Dz6voXoz.js";import"./index.es-Ll38xD3K.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
